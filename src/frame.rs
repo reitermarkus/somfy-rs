0d86706ab76545de67b55bdea9eba9f947352ff5
Markus Reiter <me@reitermark.us>
2024-06-28 22:27:40 +0200

    Fix test.

--- src/frame.rs
+++ src/frame.rs
@@ -116,3 +116,42 @@ pub trait SendFrame {
 
   fn send_frame_repeat(&mut self, frame: &Frame, repetitions: usize) -> Result<(), Self::Error>;
 }
+
+#[cfg(test)]
+mod tests {
+  use ux::u24;
+
+  use super::*;
+
+  #[test]
+  fn test_frame() {
+    let rolling_code = 42;
+    let remote_address = u24::new(0xFFAA11);
+
+    let mut frame = Frame::builder()
+      .key(0xA7)
+      .command(Command::Up)
+      .rolling_code(rolling_code)
+      .remote_address(remote_address)
+      .build()
+      .expect("Failed to build frame");
+
+    frame.deobfuscate();
+
+    let command = frame.command_and_checksum & 0b11110000;
+    let checksum = frame.command_and_checksum & 0b00001111;
+
+    assert_eq!(frame.key, 0xA7);
+    assert_eq!(command, Command::Up as u8);
+    assert_eq!(checksum, 7);
+    assert_eq!(u16::from_be_bytes(frame.rolling_code), rolling_code);
+    assert_eq!(
+      u24::new(
+        frame.remote_address[0] as u32
+          + ((frame.remote_address[1] as u32) << 8)
+          + ((frame.remote_address[2] as u32) << 16)
+      ),
+      remote_address
+    );
+  }
+}
